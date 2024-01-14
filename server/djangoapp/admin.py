42a141d7ace7218e7f54cce0ba68195f8540fb84
shane <shane@gmail.com>
2024-01-14 14:13:41 -0500

    static commit contact and about page

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,15 @@
+# djangoapp/admin.py
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1  # Number of empty forms to display for adding related CarModel objects
 
-# Register your models here.
+@admin.register(CarMake)
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
 
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('car_make', 'name', 'car_type', 'year', 'dealer_id')  # Customize the fields displayed in the admin list view
