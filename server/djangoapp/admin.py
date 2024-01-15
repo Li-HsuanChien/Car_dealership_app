436dd6198650a62be49be0badc24b91e70516802
shane <shanechien2023@gmail.com>
2024-01-15 11:34:16 -0500

    fixed adminsite bug

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,15 +1,27 @@
 # djangoapp/admin.py
 from django.contrib import admin
-from .models import CarMake, CarModel
+from .models import CarMake, CarModel, CarDealer, DealerReview
 
-class CarModelInline(admin.TabularInline):
+class CarModelInline(admin.StackedInline):
     model = CarModel
-    extra = 1  # Number of empty forms to display for adding related CarModel objects
+    extra = 2  # Number of empty forms to display for adding related CarModel objects
 
-@admin.register(CarMake)
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
 
-@admin.register(CarModel)
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ('car_make', 'name', 'car_type', 'year', 'dealer_id')  # Customize the fields displayed in the admin list view
+
+
+class CarDealerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'city', 'full_name', 'id', 'lat', 'long', 'short_name', 'st', 'zip')  # Customize the fields displayed in the admin list view
+
+
+class DealerReviewlAdmin(admin.ModelAdmin):
+    list_display = ('reviewer_name', 'review_text', 'rating')  # Customize the fields displayed in the admin list view
+
+
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarDealer, CarDealerAdmin)
+admin.site.register(DealerReview, DealerReviewlAdmin)
